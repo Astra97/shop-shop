60cc3f67dc90d080ab2e97b9ece48d6873de08e2
Astra97 <abelestrada5@yahoo.com>
2021-01-11 20:12:46 -0700

    add redux

---
+++ client/build/precache-manifest.825e273843a56688c649d742e137ab99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090291a58f7a8dbce5b062aca551d8a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82acc253daa7f198e24",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "9a2ee8b6c6e9c97af16e",
+    "url": "/static/js/2.b0094aba.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.b0094aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82acc253daa7f198e24",
+    "url": "/static/js/main.5aa875fc.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
